0c2287a07eced0e9637b9583d4b2fa85040956a7
myrual <lilin@myrual.me>
2017-04-25 09:31:05 +0000

    add json api login

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,4 +52,17 @@ class User < ApplicationRecord
   def display_name
     self.email.split("@").first
   end
+  
+   def reset_authentication_token!
+     self.authentication_token = generate_authentication_token
+     self.save
+   end
+ 
+  private
+  def generate_authentication_token
+    loop do
+      token = Devise.friendly_token
+      break token unless User.where(authentication_token: token).first
+    end
+  end
 end
